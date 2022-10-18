e4f5e158608ecf65b2c5a047ef34c1a142550f1e
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2022-10-18 11:21:13 +0530

    explored basic working of router and processor

---
+++ src/main/java/com/sk/explorecamel/basic/BasicProcessor.java
@@ -0,0 +1,16 @@
+package com.sk.explorecamel.basic;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BasicProcessor implements Processor {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		log.info("Inside processor {} ", exchange.getMessage().getBody());
+	}
+
+}
